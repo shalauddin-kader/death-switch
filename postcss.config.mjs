2696f9f804e321df695d9de36677487018c931f3
Shalauddin Kader <salauddin.kader406@gmail.com>
2025-03-10 00:45:26 +0600

    Fixed Tailwind PostCSS issue correctly

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,12 +1,10 @@
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
-import tailwindPostcss from '@tailwindcss/postcss';
 
 const config = {
   plugins: [
     tailwindcss,
-    tailwindPostcss,
-    autoprefixer,
+    autoprefixer
   ],
 };
 
